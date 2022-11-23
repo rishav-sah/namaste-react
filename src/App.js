28601a094d9953424828b653360f7e154dceb978
Rishav Sah <rishav@Rishavs-MacBook-Pro.local>
2022-11-24 01:11:45 +0530

    Assignment 2 complete

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement("h1",{className: "title"},"Hello World using React");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
