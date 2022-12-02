f5f625311559276af07bd6c87cdfcd5f384805cb
Rishav Sah <rishav@Rishavs-MacBook-Pro.local>
2022-12-02 15:46:05 +0530

    commit revert & only assignment 3 remains

--- src/App.js
+++ src/App.js
@@ -2,38 +2,18 @@ import React from "react";
 import Header from "./components/Header/Header.jsx";
 import Heading from "./components/Heading/Heading.jsx";
 import HeadingElement from "./components/HeadingElement/HeadingElement.jsx";
-import Card from "./components/Card/Card.jsx";
-import team from "./utils/userDetails.json"
 
 import "./style.css";
 
 const App = () => {
   return (
     <div className="container">
-      {/* Header with Logo */}
+    {/* Header with Logo */}
       <Header/>
-      {/* Heading Using JSX & createElement */}
-      {/* <div className="heading">
+    {/* Heading Using JSX & createElement */}
+      <div className="heading">
         <Heading/>
         <HeadingElement/>
-      </div> */}
-
-      <div className="card-container">
-        {
-          team.users.map((user) => {
-            const {id, img, name, address, organization, designation} = user;
-            return (
-              <Card
-                key={id}
-                img={img}
-                name={name}
-                address={address}
-                organization={organization}
-                designation={designation}
-              />
-            );
-          })
-        }
       </div>
     </div>
   );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,20 @@
 import React from "react";
-import logo from "../../assets/logo-web-pirates.png";
 
 const Header = () => {
   return (
     <header tabIndex={1} className="header">
-      <nav className="navbar">
-        <div className="navbar-logo">
-          <img src={logo} alt="logo" />
-        </div>
+      <div className="header-logo">
+        <img src="https://namastedev.com/namaste-react-bootcamp/assets/img/small-logo.png" alt="logo" />
+      </div>
 
-        <div className="navbar-searchbar">
-          <input type="text" name="searchInput" placeholder="What are you searching for?"/>
-          <button>Search</button>
-        </div>
+      <div className="header-searchbar">
+        <input type="text" name="searchInput" placeholder="What are you searching for?"/>
+        <button>Search</button>
+      </div>
 
-        <div className="navbar-user">
-          <img src="https://img.icons8.com/cotton/512/gender-neutral-user.png" alt="user icon" />
-        </div>
-      </nav>
+      <div className="header-user">
+        <img src="https://img.icons8.com/cotton/512/gender-neutral-user.png" alt="user icon" />
+      </div>
     </header>
   );
 };
