1d0a17e50ad0681944b42695ebf9fdb9e543fcaa
Rishav Sah <rishav@Rishavs-MacBook-Pro.local>
2022-12-02 15:50:17 +0530

    Card Component rendered

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,23 @@
 import React from "react";
+import logo from "../../assets/logo-web-pirates.png";
 
 const Header = () => {
   return (
     <header tabIndex={1} className="header">
-      <div className="header-logo">
-        <img src="https://namastedev.com/namaste-react-bootcamp/assets/img/small-logo.png" alt="logo" />
-      </div>
+      <nav className="navbar">
+        <div className="navbar-logo">
+          <img src={logo} alt="logo" />
+        </div>
 
-      <div className="header-searchbar">
-        <input type="text" name="searchInput" placeholder="What are you searching for?"/>
-        <button>Search</button>
-      </div>
+        <div className="navbar-searchbar">
+          <input type="text" name="searchInput" placeholder="What are you searching for?"/>
+          <button>Search</button>
+        </div>
 
-      <div className="header-user">
-        <img src="https://img.icons8.com/cotton/512/gender-neutral-user.png" alt="user icon" />
-      </div>
+        <div className="navbar-user">
+          <img src="https://img.icons8.com/cotton/512/gender-neutral-user.png" alt="user icon" />
+        </div>
+      </nav>
     </header>
   );
 };
